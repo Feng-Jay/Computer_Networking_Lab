79958d3cc539c0a905dff2ec01e676e620fc20f8
Feng-Jay <2652925120@qq.com>
2021-05-29 23:21:55 +0800

    day2

---
+++ ChatRoom/target/rls/debug/build/alsa-sys-8184b0cd66798c9d/build_script_build-8184b0cd66798c9d.d
@@ -0,0 +1,5 @@
+d:\grade2\计算机网络\Socket\ChatRoom\target\rls\debug\build\alsa-sys-8184b0cd66798c9d\build_script_build-8184b0cd66798c9d.exe: C:\Users\DELL\.cargo\registry\src\github.com-1ecc6299db9ec823\alsa-sys-0.3.1\build.rs
+
+d:\grade2\计算机网络\Socket\ChatRoom\target\rls\debug\build\alsa-sys-8184b0cd66798c9d\build_script_build-8184b0cd66798c9d.d: C:\Users\DELL\.cargo\registry\src\github.com-1ecc6299db9ec823\alsa-sys-0.3.1\build.rs
+
+C:\Users\DELL\.cargo\registry\src\github.com-1ecc6299db9ec823\alsa-sys-0.3.1\build.rs:
